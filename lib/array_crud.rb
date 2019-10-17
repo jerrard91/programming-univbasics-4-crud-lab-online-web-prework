0403a903dbfec83dae5da469182cf0549a2d72da
jerrard suell <seulljay@gmail.com>
2019-10-17 18:39:21 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,7 @@ end
 def add_element_to_end_of_array(array, element)
   food = ["taco", "burger", "salad", "corn"]
   food.push("hotdog")
-  p add_element_to_end_of_array(array, element)
+  
 end
 
 def add_element_to_start_of_array(array, element)
